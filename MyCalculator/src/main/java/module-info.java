88229b257ed9d9c6b2e22add9831ca9f19fd0e1b
Shahrier Shanto <126169888+akira2049@users.noreply.github.com>
2024-06-08 12:10:29 +0600

    Delete MyCalculator directory

--- MyCalculator/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module cse213.tutorialprojects.calculator {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens cse213.tutorialprojects.calculator to javafx.fxml;
-    exports cse213.tutorialprojects.calculator;
-}
